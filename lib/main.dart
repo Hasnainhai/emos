0cfb0ef542cb6daf2424f24b3cd1ec1511d23989
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2023-11-04 17:09:20 +0500

    working on HomeView Features

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(),
       onGenerateRoute: Routes.generateRoute,
 
-      initialRoute: RouteName.findhosptialview,
+      initialRoute: RouteName.homeView,
       // home: const RegisterView(),
     );
   }
