d3fb1aa7a5189dea9ba0a9dfe7edd8a084302793
Basit ali <basitalyshah51214@gmail.com>
2023-11-14 04:13:16 -0800

    add clinic card and doctor at home card

--- lib/view/ClinicView/Widgets/clinic_list_view.dart
+++ lib/view/ClinicView/Widgets/clinic_list_view.dart
@@ -1,8 +1,6 @@
 import 'package:emos/components/RoundedButton/rounded_button.dart';
 import 'package:emos/components/VerticalSpacing/vertical_spacing.dart';
 import 'package:emos/res/GlobalColors/colors.dart';
-import 'package:emos/routes/routes_name.dart';
-import 'package:emos/view/HosptialListView/Widgets/owner.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -15,7 +13,7 @@ class ClinicListView extends StatelessWidget {
     return Column(
       children: [
         Container(
-          height: (MediaQuery.of(context).size.height / 1.83) + 1.8,
+          height: (MediaQuery.of(context).size.height / 3),
           width: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(
@@ -26,83 +24,121 @@ class ClinicListView extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
-                height: 210,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(
-                    16,
-                  ),
-                  image: const DecorationImage(
-                    image: AssetImage(
-                      "images/booking.jpg",
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                    height: 72,
+                    width: 72,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        8,
+                      ),
+                      image: const DecorationImage(
+                        image: AssetImage(
+                          "images/booking.jpg",
+                        ),
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                    fit: BoxFit.cover,
                   ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: Column(
+                  Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      //  here goverment
-                      OwnerWidget(
-                        circleColor: const Color(0xff5ABF24),
-                        owner: "Government",
+                      Text(
+                        "Jonhs Hokins Clinic",
+                        style: GoogleFonts.getFont(
+                          "Roboto",
+                          textStyle: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: AppColor.textColor,
+                          ),
+                        ),
+                      ),
+                      const VerticalSpeacing(6),
+                      Text(
+                        "23 Estean, New York City, USA",
+                        style: GoogleFonts.getFont(
+                          "Roboto",
+                          textStyle: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: AppColor.bgFillColor,
+                          ),
+                        ),
+                      ),
+                      const VerticalSpeacing(6),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                            size: 14,
+                          ),
+                          const SizedBox(
+                            width: 4,
+                          ),
+                          Text(
+                            "4.8 (456 Reviews)",
+                            style: GoogleFonts.getFont(
+                              "Roboto",
+                              textStyle: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: AppColor.bgFillColor,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20, top: 16),
-                child: Text(
-                  "Jonhs Hokins Hospital",
-                  style: GoogleFonts.getFont(
-                    "Roboto",
-                    textStyle: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      color: AppColor.textColor,
-                    ),
-                  ),
-                ),
+                  )
+                ],
               ),
               Padding(
                 padding: const EdgeInsets.only(
                   left: 20,
-                ),
-                child: Text(
-                  "23 Estean, New York City, USA",
-                  style: GoogleFonts.getFont(
-                    "Roboto",
-                    textStyle: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      color: AppColor.bgFillColor,
-                    ),
-                  ),
-                ),
-              ),
-              const VerticalSpeacing(4),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 20,
+                  right: 20,
+                  top: 12,
                 ),
                 child: Row(
                   children: [
                     const Icon(
-                      Icons.star,
-                      color: Colors.amber,
-                      size: 14,
+                      Icons.av_timer_rounded,
+                      color: AppColor.textFieldColor,
                     ),
                     const SizedBox(
-                      width: 4,
+                      width: 8,
                     ),
                     Text(
-                      "4.8 (456 Reviews)",
+                      "Working From 08:30 AM to 04:30 PM",
+                      style: GoogleFonts.getFont(
+                        "Roboto",
+                        textStyle: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: AppColor.bgFillColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 20, right: 20, top: 12, bottom: 8),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.send,
+                      color: AppColor.textFieldColor,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      "2.5 Km from your location",
                       style: GoogleFonts.getFont(
                         "Roboto",
                         textStyle: const TextStyle(
@@ -129,50 +165,6 @@ class ClinicListView extends StatelessWidget {
                 ),
               ),
               const VerticalSpeacing(20),
-              Row(
-                children: [
-                  Container(
-                    height: 46,
-                    width: MediaQuery.of(context).size.width / 2 - 21,
-                    decoration: const BoxDecoration(
-                      color: Color(0xff5790B9),
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(12),
-                      ),
-                    ),
-                    child: const Center(
-                      child: Icon(
-                        Icons.send,
-                        color: AppColor.whiteColor,
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.pushNamed(
-                        context,
-                        RouteName.callview,
-                      );
-                    },
-                    child: Container(
-                      height: 46,
-                      width: MediaQuery.of(context).size.width / 2 - 21,
-                      decoration: const BoxDecoration(
-                        color: Color(0xff5ABF24),
-                        borderRadius: BorderRadius.only(
-                          bottomRight: Radius.circular(12),
-                        ),
-                      ),
-                      child: const Center(
-                        child: Icon(
-                          Icons.phone,
-                          color: AppColor.whiteColor,
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              )
             ],
           ),
         ),
--- lib/view/DrAtHomeView/Widgets/dr_list_view.dart
+++ lib/view/DrAtHomeView/Widgets/dr_list_view.dart
@@ -15,7 +15,7 @@ class DrListView extends StatelessWidget {
     return Column(
       children: [
         Container(
-          height: (MediaQuery.of(context).size.height / 1.83) + 1.8,
+          height: (MediaQuery.of(context).size.height / 2),
           width: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(
@@ -26,83 +26,119 @@ class DrListView extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
-                height: 210,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(
-                    16,
-                  ),
-                  image: const DecorationImage(
-                    image: AssetImage(
-                      "images/booking.jpg",
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                    height: 72,
+                    width: 72,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        8,
+                      ),
+                      image: const DecorationImage(
+                        image: AssetImage(
+                          "images/doctor.jpg",
+                        ),
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                    fit: BoxFit.cover,
                   ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: Column(
+                  Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      //  here goverment
-                      OwnerWidget(
-                        circleColor: const Color(0xff5ABF24),
-                        owner: "Government",
+                      Text(
+                        "Jonhs Hokins Clinic",
+                        style: GoogleFonts.getFont(
+                          "Roboto",
+                          textStyle: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: AppColor.textColor,
+                          ),
+                        ),
+                      ),
+                      const VerticalSpeacing(6),
+                      Text(
+                        "23 Estean, New York City, USA",
+                        style: GoogleFonts.getFont(
+                          "Roboto",
+                          textStyle: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: AppColor.bgFillColor,
+                          ),
+                        ),
+                      ),
+                      const VerticalSpeacing(6),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                            size: 14,
+                          ),
+                          const SizedBox(
+                            width: 4,
+                          ),
+                          Text(
+                            "4.8 (456 Reviews)",
+                            style: GoogleFonts.getFont(
+                              "Roboto",
+                              textStyle: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: AppColor.bgFillColor,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
-                  ),
-                ),
+                  )
+                ],
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 20, top: 16),
-                child: Text(
-                  "Jonhs Hokins Hospital",
-                  style: GoogleFonts.getFont(
-                    "Roboto",
-                    textStyle: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      color: AppColor.textColor,
-                    ),
-                  ),
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 20,
+                ),
+                child: RoundedButton(
+                  title: "Book an Appointment",
+                  onpress: fun,
+                  bgColor: Colors.transparent,
+                  titleColor: AppColor.bgFillColor,
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.only(
                   left: 20,
-                ),
-                child: Text(
-                  "23 Estean, New York City, USA",
-                  style: GoogleFonts.getFont(
-                    "Roboto",
-                    textStyle: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      color: AppColor.bgFillColor,
-                    ),
-                  ),
-                ),
-              ),
-              const VerticalSpeacing(4),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 20,
+                  right: 20,
+                  top: 12,
                 ),
                 child: Row(
                   children: [
                     const Icon(
-                      Icons.star,
-                      color: Colors.amber,
-                      size: 14,
+                      Icons.language,
+                      color: AppColor.textFieldColor,
                     ),
                     const SizedBox(
-                      width: 4,
+                      width: 8,
                     ),
                     Text(
-                      "4.8 (456 Reviews)",
+                      "Languages:",
+                      style: GoogleFonts.getFont(
+                        "Roboto",
+                        textStyle: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                          color: AppColor.textColor,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "English, Hindi",
                       style: GoogleFonts.getFont(
                         "Roboto",
                         textStyle: const TextStyle(
@@ -119,60 +155,183 @@ class DrListView extends StatelessWidget {
                 padding: const EdgeInsets.only(
                   left: 20,
                   right: 20,
-                  top: 20,
+                  top: 12,
                 ),
-                child: RoundedButton(
-                  title: "Book an Appointment",
-                  onpress: fun,
-                  bgColor: Colors.transparent,
-                  titleColor: AppColor.bgFillColor,
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.location_on_outlined,
+                      color: AppColor.textFieldColor,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      "Location:",
+                      style: GoogleFonts.getFont(
+                        "Roboto",
+                        textStyle: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                          color: AppColor.textColor,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "23 Estean, New York City, USA",
+                      style: GoogleFonts.getFont(
+                        "Roboto",
+                        textStyle: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: AppColor.bgFillColor,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              const VerticalSpeacing(20),
-              Row(
-                children: [
-                  Container(
-                    height: 46,
-                    width: MediaQuery.of(context).size.width / 2 - 21,
-                    decoration: const BoxDecoration(
-                      color: Color(0xff5790B9),
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(12),
-                      ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 12,
+                ),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.person,
+                      color: AppColor.textFieldColor,
                     ),
-                    child: const Center(
-                      child: Icon(
-                        Icons.send,
-                        color: AppColor.whiteColor,
-                      ),
+                    const SizedBox(
+                      width: 8,
                     ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.pushNamed(
-                        context,
-                        RouteName.callview,
-                      );
-                    },
-                    child: Container(
-                      height: 46,
-                      width: MediaQuery.of(context).size.width / 2 - 21,
-                      decoration: const BoxDecoration(
-                        color: Color(0xff5ABF24),
-                        borderRadius: BorderRadius.only(
-                          bottomRight: Radius.circular(12),
-                        ),
-                      ),
-                      child: const Center(
-                        child: Icon(
-                          Icons.phone,
-                          color: AppColor.whiteColor,
+                    Text(
+                      "Degrees:",
+                      style: GoogleFonts.getFont(
+                        "Roboto",
+                        textStyle: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                          color: AppColor.textColor,
                         ),
                       ),
                     ),
-                  )
-                ],
-              )
+                    Text(
+                      "Master’s degree, Endocrinology",
+                      style: GoogleFonts.getFont(
+                        "Roboto",
+                        textStyle: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: AppColor.bgFillColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(
+                color: AppColor.textFieldColor,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 12,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.work_outline_outlined,
+                          color: AppColor.textFieldColor,
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          "Work Experience",
+                          style: GoogleFonts.getFont(
+                            "Roboto",
+                            textStyle: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              color: AppColor.textColor,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Icon(
+                      Icons.navigate_next,
+                      color: AppColor.textColor2,
+                    )
+                  ],
+                ),
+              ),
+              const VerticalSpeacing(8),
+              const Divider(
+                color: AppColor.textFieldColor,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 12,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.attach_money_sharp,
+                          color: AppColor.textFieldColor,
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          "Consultant Fees:",
+                          style: GoogleFonts.getFont(
+                            "Roboto",
+                            textStyle: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              color: AppColor.textColor,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 20,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: AppColor.bgFillColor,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "₹350",
+                          style: GoogleFonts.getFont(
+                            "Roboto",
+                            textStyle: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              color: AppColor.whiteColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const VerticalSpeacing(8),
             ],
           ),
         ),
