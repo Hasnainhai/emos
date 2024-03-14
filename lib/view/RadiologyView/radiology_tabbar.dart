ebfee98496ba96a6d60dab7c6ea15633d52e3b96
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-03-14 15:48:50 +0500

    fix some issues

--- lib/view/RadiologyView/radiology_tabbar.dart
+++ lib/view/RadiologyView/radiology_tabbar.dart
@@ -1,7 +1,6 @@
 import 'package:emos/components/VerticalSpacing/vertical_spacing.dart';
 import 'package:emos/res/GlobalColors/colors.dart';
 import 'package:emos/routes/routes_name.dart';
-import 'package:emos/view/HosptialListView/filter_hosptial.dart';
 import 'package:emos/view/HosptialListView/Widgets/owner.dart';
 import 'package:emos/view/RadiologyView/Widgets/radiology_filter.dart';
 import 'package:emos/view/RadiologyView/Widgets/radiology_list_view.dart';
