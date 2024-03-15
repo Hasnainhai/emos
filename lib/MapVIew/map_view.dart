0e55f45ca8a12156362335339fc77a8c6bf15fb2
Basit ali <basitalyshah51214@gmail.com>
2024-03-15 12:03:00 -0700

    changes to fix the issues of the app

--- lib/MapVIew/map_view.dart
+++ lib/MapVIew/map_view.dart
@@ -1,14 +1,34 @@
+import 'package:emos/res/GlobalColors/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MapConfigView extends StatelessWidget {
   const MapConfigView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Map Here...'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 0,
+          title: Text(
+            "Emergency",
+            style: GoogleFonts.getFont(
+              "Roboto",
+              textStyle: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+                color: AppColor.textColor,
+              ),
+            ),
+          ),
+        ),
+        body: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [],
+          ),
+        )));
   }
 }
